b5615eb8cb5e160ac8721cb8a6ec78eb9a4584b4
Rupantar <rupantaradhikary2002@gmail.com>
2023-03-10 22:36:23 +0530

    model loding working but has some issues ahy box loaded

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 // setting up canvas
 const canvas = document.getElementById("canvas");
@@ -10,28 +12,42 @@ const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
 // camera settings
 const fov = 45;
 const aspect_ratio = canvas.width / canvas.height;
-const near = 0.1;
-const far = 100;
+const near = 100;
+const far = 10000;
 const camera = new THREE.PerspectiveCamera(fov, aspect_ratio, near, far);
-camera.position.z = 2;
+camera.position.set(600, 600, 600);
+camera.lookAt(0, 0, 0);
 // scene
 const scene = new THREE.Scene();
-// testure loader
-const textureLoader = new THREE.TextureLoader();
 
-// adding objects
-// TODO testing
-const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-const material = new THREE.MeshBasicMaterial({
-  color: 0xfffffff,
-  map: textureLoader.load("images/lava_texture.png"),
+// skybox
+scene.background = new THREE.CubeTextureLoader()
+  .setPath("asset/cubemaps/")
+  .load(["px.png", "nx.png", "py.png", "ny.png", "pz.png", "nz.png"]);
+
+// light
+const dirLight = new THREE.DirectionalLight();
+dirLight.position.set(0, 800, 0);
+dirLight.color.set(0xffffff);
+dirLight.intensity = 1;
+const dirLightHelper = new THREE.DirectionalLightHelper(
+  dirLight,
+  100,
+  0xff0000
+);
+scene.add(dirLight);
+scene.add(dirLightHelper);
+
+// load model
+const gltfLoader = new GLTFLoader();
+gltfLoader.load("asset/scene/scene.gltf", (model) => {
+  const root = model.scene;
+  scene.add(root);
 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
-// CreateTexturedObject(0, 0, 0, "images/lava_texture.png");
 
-// lighting
-AddDirectionalLight(-1, 2, 4, 1, 0xffffff);
+// orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
+
 // rendering
 Render();
 
@@ -44,26 +60,9 @@ function ResizeWindow() {
 }
 
 function Render(time) {
-  mesh.rotation.set(0, time * 0.001, 0);
+  controls.update();
   renderer.clearColor();
   renderer.clearDepth();
   renderer.render(scene, camera);
   requestAnimationFrame(Render);
-}
-
-function AddDirectionalLight(x, y, z, intencity, color) {
-  const light = new THREE.DirectionalLight(color, intencity);
-  light.position.set(x, y, z);
-  scene.add(light);
-}
-
-function CreateTexturedObject(x, y, z, filepath) {
-  const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-  const material = new THREE.MeshBasicMaterial({
-    color: 0xfffffff,
-    map: textureLoader.load(filepath),
-  });
-  const mesh = new THREE.Mesh(geometry, material);
-  mesh.position.set(x, y, z);
-  scene.add(mesh);
 }
